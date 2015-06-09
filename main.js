05bbac04b0eaa09c534736578d7b3231ccfbeddf
Christian Münch <c.muench@outlook.de>
2015-06-09 10:25:02 +0200

    Background and Animationtest
    First graphics and simple animation-test with dummy divs.

---
+++ main.js
@@ -0,0 +1,13 @@
+// DEBUGGER
+console.log("jsLoadTrue");
+
+// VARIABLES
+
+
+// 	OBJECTS
+var containerCrane	= {ID:null, mode:"idle"};
+
+var containerShip	= {ID:null, mode:"idle"};
+
+
+document.getElementById("container").style.backgroundColor = "blue";
